a3ac00aa3faf24142684bd9a035786cf149dd662
Paneri Patel <paneripatel@Paneris-MacBook-Pro-6.local>
2025-08-20 15:42:14 -0400

    Done DP-2

---
+++ 2.py
@@ -0,0 +1,22 @@
+'''
+Problem2 (https://leetcode.com/problems/coin-change-2/)
+'''
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int: # type: ignore
+        if coins == None or len(coins) == 0:
+            return 0
+        n = len(coins)    
+        
+        dp = [[0] * (amount+1)] * (n+1)
+        for i in range(1, n+1):
+            dp[i][0] = 1
+
+        for i in range(1, n+1):
+            for j in range(1, amount+1):
+                if j < coins[i-1]:
+                    dp[i][j] = dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j - coins[i-1]]
+
+        return dp[n][amount]            
